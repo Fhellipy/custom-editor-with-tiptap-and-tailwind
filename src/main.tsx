e2c129e0499948f61345b48b9cde770512457783
Fhellipy Santana <fhellipy.santana@dantaseletro.com.br>
2023-10-20 17:57:10 -0300

    feat: adiciona estilos

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./app.tsx";
+import "./globals.css";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
