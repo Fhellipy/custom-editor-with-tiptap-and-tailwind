0c910d017313f46ca95aa30f760b672863f10d45
Fhellipy Santana <fhellipy.santana@dantaseletro.com.br>
2023-10-23 16:05:53 -0300

    feat: adiciona componente de layout

---
+++ src/shared/layouts/layouts.tsx
@@ -0,0 +1,14 @@
+import { Header } from "@/shared/components";
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+export function Layout({ children }: LayoutProps) {
+  return (
+    <div className="h-full w-full overflow-auto p-4">
+      <Header />
+      {children}
+    </div>
+  );
+}
