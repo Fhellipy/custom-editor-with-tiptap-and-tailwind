35a1abfaad5ea9be012bb70f8e56b57ac654e3dd
Fhellipy Santana <fhellipy.santana@dantaseletro.com.br>
2023-10-23 16:02:44 -0300

    feat: configura o alias do vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  resolve:{
+    alias: [
+      {
+        find: "@",
+        replacement: resolve("src"),
+      }
+    ],
   },
 })
