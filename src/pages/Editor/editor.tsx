1c2eaede3fd7b9f66fd2489adc1525b666ee9414
Fhellipy Santana <fhellipy.santana@dantaseletro.com.br>
2023-10-28 12:10:38 -0300

    feat: adiciona componente do editor

--- src/pages/Editor/editor.tsx
+++ src/pages/Editor/editor.tsx
@@ -1,7 +1,11 @@
-export function Editor() {
+import { EditorComponent } from "./components";
+
+export default function EditorPage() {
   return (
-    <article className="h-full border rounded p-4">
+    <article className="w-full h-full border rounded p-4">
       <h1 className="font-bold uppercase">Editor</h1>
+
+      <EditorComponent />
     </article>
   );
 }
