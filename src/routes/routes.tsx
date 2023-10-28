d69255f82586220f7d20a7fbb7066e04df84c306
Fhellipy Santana <fhellipy.santana@dantaseletro.com.br>
2023-10-28 12:19:44 -0300

    feat: adiciona a página lazy da paleta de cores

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,12 @@
-import { ColorPalette } from "@/pages";
 import { Routes as AllRoutes, Route } from "react-router-dom";
 import { Editor } from "./Editor";
+import { Palette } from "./Palette";
 
 export function Routes() {
   return (
     <AllRoutes>
       <Route path="/" element={<Editor />} />
-      <Route path="/colors-palette" element={<ColorPalette />} />
+      <Route path="/colors-palette" element={<Palette />} />
     </AllRoutes>
   );
 }
