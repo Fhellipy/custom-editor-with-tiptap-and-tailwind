3dbed2f0bc33a882a7578fd96a9263eac2e50dd2
Fhellipy Santana <fhellipy.santana@dantaseletro.com.br>
2023-10-30 17:59:19 -0300

    refactor: importa as páginas do projeto

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,11 @@
+import { ColorPalette, EditorPage } from "@/pages";
 import { Routes as AllRoutes, Route } from "react-router-dom";
-import { Editor } from "./Editor";
-import { Palette } from "./Palette";
 
 export function Routes() {
   return (
     <AllRoutes>
-      <Route path="/" element={<Editor />} />
-      <Route path="/colors-palette" element={<Palette />} />
+      <Route path="/" element={<EditorPage />} />
+      <Route path="/colors-palette" element={<ColorPalette />} />
     </AllRoutes>
   );
 }
