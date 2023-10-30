16e8bbdf152997ce3e03d3822093aa55fffc3103
Fhellipy Santana <fhellipy.santana@dantaseletro.com.br>
2023-10-30 17:58:46 -0300

    remove: apaga arquivos desnecessários

--- src/routes/Editor/index.tsx
+++
@@ -1,6 +0,0 @@
-import { lazy } from "react";
-export const LazyEditorPage = lazy(() => import("@/pages/Editor/editor"));
-
-export function Editor() {
-  return <LazyEditorPage />;
-}
--- src/routes/Palette/index.tsx
+++
@@ -1,8 +0,0 @@
-import { lazy } from "react";
-export const LazyPalettePage = lazy(
-  () => import("@/pages/ColorPalette/color-palette"),
-);
-
-export function Palette() {
-  return <LazyPalettePage />;
-}
