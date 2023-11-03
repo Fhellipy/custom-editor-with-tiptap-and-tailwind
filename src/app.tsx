50dac19d0fb64aa102e7b3e25a921acb32a45b7a
Fhellipy Santana <fhellipy.santana@dantaseletro.com.br>
2023-11-03 17:52:51 -0300

    feat: adiciona a biblioteca de notificação (toast)

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,12 @@
-import { BrowserRouter } from "react-router-dom";
 import { Routes } from "@/routes";
 import { Layout } from "@/shared/layouts";
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter } from "react-router-dom";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Toaster position="bottom-right" />
       <Layout>
         <Routes />
       </Layout>
