6faed80d65b8522aaa9797db5e6d31bf784aad1e
Fhellipy Santana <fhellipy.santana@dantaseletro.com.br>
2023-11-04 12:12:56 -0300

    feat: adiciona variação de estilo

--- src/pages/Editor/components/Buttons/button-menu.tsx
+++ src/pages/Editor/components/Buttons/button-menu.tsx
@@ -7,11 +7,12 @@ type ButtonMenuProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   title: string;
   disabled?: boolean;
   active?: boolean;
+  variant?: boolean;
   className?: ClassValue;
 };
 
 function Button(props: ButtonMenuProps, ref: React.Ref<HTMLButtonElement>) {
-  const { icon, title, active, disabled, onClick } = props;
+  const { icon, title, active, disabled, variant, onClick } = props;
 
   return (
     <button
@@ -23,6 +24,10 @@ function Button(props: ButtonMenuProps, ref: React.Ref<HTMLButtonElement>) {
         "w-fit h-10 bg-surface text-surface-foreground py-2 px-4 border rounded hover:bg-primary hover:text-primary-foreground transition-colors duration-200 ease-in-out",
         { "bg-primary text-primary-foreground": active },
         { "w-full h-fit": !icon && title },
+        {
+          "h-fit border px-4 py-2 text-xs font-bold text-primary-foreground bg-primary rounded hover:bg-surface hover:text-surface-foreground transition-colors duration-200 ease-in-out":
+            variant,
+        },
         props.className,
       )}
     >
