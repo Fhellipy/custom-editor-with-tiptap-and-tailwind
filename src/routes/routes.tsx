43e807659ea5ac89dfe9b430949c16d5f21ba5d8
Fhellipy Santana <fhellipy.santana@dantaseletro.com.br>
2023-11-04 08:55:28 -0300

    refactor: adiciona função de remover pqina link

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,12 @@ import { ColorPalette, EditorPage } from "@/pages";
 import { Routes as AllRoutes, Route } from "react-router-dom";
 
 export function Routes() {
+  // Remove pqina link
+  const a = document.querySelector("a");
+  const isPqina = a?.innerHTML === "for use on pqina.nl only";
+
+  if (isPqina) a?.remove();
+
   return (
     <AllRoutes>
       <Route path="/" element={<EditorPage />} />
